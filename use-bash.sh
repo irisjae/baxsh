866ea46d2df4a86fc1bd1ba478b63ed61e77c32c
mumenrider <brian.aquinde@outlook.com>
2017-12-20 04:51:32 +0000

    checkpoint
    added screen config

--- use-bash.sh
+++ use-bash.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 function lls { 
-	ls -1 --group-directories-first ${@}
+	ls -1 --group-directories-first "${@}"
 }
 
 function vvim {
-	vim "+set shortmess=I" "+color murphy" "+set nowrap" ${@}
+	vim "+set shortmess=I" "+color murphy" "+set nowrap" "${@}"
+}
+
+__screen_config=$(mktemp)
+echo "layout save default" > ${__screen_config}
+function sscreen {
+	screen -c "$__screen_config" "${@}"
 }
